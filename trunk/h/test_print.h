2c20d9b92badbd89f183d9cf3547b041697be579
Juchan Kim <juckim@blackberry.com>
2019-12-17 10:28:35 -0500

    reconstructed master added bootloader.S

--- trunk/h/test_print.h
+++
@@ -1,2 +0,0 @@
-void test_print_pass(const char *func);
-void test_print_fail(int exp, int actual,const char *func);
--- trunk/kernel/clock.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLOCK_H
-#define CLOCK_H
-
-extern int lost_clock_ticks;
-extern long realtime;
-
-int clock_task();
-
-#endif /* CLOCK_H */
